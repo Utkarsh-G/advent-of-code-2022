3dc32492852fc39837cc92c32991464926f1d3e9
Utkarsh Gaur <charmedtobe@gmail.com>
2022-12-07 22:17:05 -0500

    working code

--- 7_out_of_space/drive_calc.py
+++ 7_out_of_space/drive_calc.py
@@ -12,6 +12,18 @@ class Node:
 root_node = Node("/")
 temp_parent_node = root_node
 
+def propagate_sizes(node, prop_size):
+	is_root = node.name == "/"
+	if not is_root:
+		parent_node = node.parent
+		parent_node.size += prop_size
+		print(f"prop parent {parent_node.name} : {parent_node.size}")
+		propagate_sizes(parent_node, prop_size)
+	else:
+		print(f"prop / : {node.size}")
+		return
+
+
 with open(sys.argv[1], 'r') as f:
 	line = f.readline()
 	line = f.readline()
@@ -25,7 +37,7 @@ with open(sys.argv[1], 'r') as f:
 			sum = 0
 			for num in sizes:
 				sum += num
-			temp_parent_node.size += sum
+			#temp_parent_node.size += sum
 			print(f"Node: {temp_parent_node.name} size: {temp_parent_node.size}")
 		elif parsed_line[0]=="$" and parsed_line[1] == "ls":
 			print(f"Showing ls of {temp_parent_node.name}")
@@ -41,14 +53,28 @@ with open(sys.argv[1], 'r') as f:
 				node.parent = temp_parent_node
 				temp_parent_node.children.append(node)
 				print(f"Leaf name: {node.name} size: {node.size}")
+				propagate_sizes(node, node.size)
 		line = f.readline()
 
-while temp_parent_node.name != "/":
+is_not_root = True
+
+def propagate_sizes(node):
+	is_root = node.name == "/"
+	parent_node = node.parent
+	parent_node.size += node.size
+	if not is_root:
+		print(f"prop parent {parent_node.name} : {parent_node.size}")
+		propagate_sizes(parent_node)
+	else:
+		print(f"prop / : {node.size}")
+		return
+
+while is_not_root: 
 	temp_parent_node = temp_parent_node.parent
 	sizes = [x.size for x in temp_parent_node.children]
 	sum = 0
 	for num in sizes:
 		sum += num
-	temp_parent_node.size += sum
+	#temp_parent_node.size += sum
 	print(f"Node: {temp_parent_node.name} size: {temp_parent_node.size}")
-
+	is_not_root = temp_parent_node.name != "/"
