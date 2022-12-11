09b5ec0661094979f5ea2306ac7cb226a262b6e7
Utkarsh Gaur <charmedtobe@gmail.com>
2022-12-10 22:48:26 -0500

    day 10, 1st works, 2nd almost there

---
+++ 10_cathode_ray_tube/2_sprite.py
@@ -0,0 +1,33 @@
+import sys
+
+turn = 0
+signal = 1
+
+def print_20s(turn, signal, render):
+	is_draw = True if (turn%40) >= signal and (turn%40) <= (signal+2) else False
+	render += "#" if is_draw else "." #if turn == 20 or turn == 60 or turn == 100 or turn == 140 or turn == 180 or turn == 220:
+		#print(signal)
+	
+		#print(render)
+	if turn > 40 and (turn%40 == 1):
+		print(render)
+	return render
+
+with open(sys.argv[1], 'r') as f:
+	line = f.readline()
+	render = ""
+	while line:
+		instruction = line.strip().split(" ")
+		
+		if instruction[0]=="noop":
+			turn += 1
+			render = print_20s(turn, signal, render)
+		
+		if instruction[0]=="addx":
+			for i in range(2):
+				turn += 1
+				render = print_20s(turn, signal, render)
+			signal += int(instruction[1])
+
+		line = f.readline()
+		
