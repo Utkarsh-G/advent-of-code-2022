8994fb12d897d5826caf050579e0c00d08e24ea5
Utkarsh Gaur <charmedtobe@gmail.com>
2022-12-17 12:27:24 -0500

    monkey processing

--- 11_monkey_business/1_monkey_toss.py
+++ 11_monkey_business/1_monkey_toss.py
@@ -5,7 +5,12 @@ def process_monkey_text(text):
     split_text = text.split("\n")
     monkey_number = int(split_text[0].split(" ")[1][0])
     starting_items =  [int(x) for x in split_text[1].strip().split(": ")[1].split(", ")]
-    print(starting_items)
+    worry_operation = split_text[2].strip().split("= ")[1]
+    divisibility_test = int(split_text[3].strip().split(" ")[-1])
+    true_target = int(split_text[4].strip().split(" ")[-1])
+    false_target = int(split_text[5].strip().split(" ")[-1])
+
+    return {starting_items, worry_operation, divisibility_test, true_target, false_target}
 
 with open(sys.argv[1], 'r') as f:
     text = f.read()
